52cbf0105afaefcca3191b948ecec23c4235d40b
Josh Barrow <josh@jukaela.com>
2013-01-07 10:37:14 -0600

    Further work on the placeholder settings

--- src/com/jukaela/Jukaela/SettingsActivity.java
+++ src/com/jukaela/Jukaela/SettingsActivity.java
@@ -1,23 +1,16 @@
 package com.jukaela.Jukaela;
 
-import android.annotation.TargetApi;
-import android.content.Context;
-import android.content.res.Configuration;
+import android.graphics.Color;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
-
-import java.util.List;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -25,6 +18,12 @@ public class SettingsActivity extends PreferenceActivity {
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
 
+		System.out.println("Inside SettingsActivity");
+		
+		setTheme(android.R.style.Theme_Light);
+		
+		setTitle("Settings");
+		
 		setupSimplePreferencesScreen();
 	}
 
